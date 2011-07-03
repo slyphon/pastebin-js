30433ffc8d327b86252a6f012747e34f7ae4dd75
slyphon <slyphon@gmail.com>
2011-07-03 00:55:28 +0000

    cargo-culting config from topper

---
+++ app.js
@@ -0,0 +1,36 @@
+require("coffee-script");
+require.paths.unshift('./node_modules');
+
+/**
+ * dependencies
+*/
+
+var express = require('express');
+
+var app = module.exports = express.createServer();
+var _ = require('underscore');
+
+/**
+ * configuration
+*/
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.set('port', 3001);
+  app.set("mongodb", {
+      hosname: 'localhost',
+      port: 27017,
+      db: "pastebin_development"
+  })
+});
+
+
